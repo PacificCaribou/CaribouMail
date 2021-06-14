968935276b5ec58fa67c62956322eeae5566478e
Jefferson Chen <me@chenjefferson.com>
2021-06-14 10:48:28 -0700

    add uploads support for client and backend

--- server/main.go
+++ server/main.go
@@ -2,18 +2,76 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-func uploadFile(w http.ResponseWriter, r *http.Request) {
-  fmt.Println("File Upload Endpoint Hit")
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "GET" {
+    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    return
+  }
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    return
+  }
+
+  fmt.Println("Uploading file...")
+
+  r.ParseMultipartForm(10 << 20)
+
+  file, _, err := r.FormFile("myFile")
+  if err != nil {
+    fmt.Printf("While reading user file - %s", err)
+    return
+  }
+  defer file.Close()
+
+  // ensure uploads dir exists
+  fileInfo, err := os.Stat("uploads")
+  if os.IsNotExist(err) {
+    // make dir if file DNE
+    err := os.Mkdir("uploads", 0755)
+    if (err != nil) {
+      fmt.Println("Error: while making temp dir: ", err)
+      return
+    }
+  } else if (err != nil) {
+    fmt.Println("Error while opening temp dir: ", err)
+    return
+  } else if (! fileInfo.IsDir()) {
+    fmt.Printf("File %s is not a directory\n", fileInfo.Name())
+    return
+  }
+
+  // read user file and write to disk
+  tmpFile, err := ioutil.TempFile("uploads", "upload-*")
+  if err != nil {
+    fmt.Println("Error while opening temp file: ", err)
+    return
+  }
+  defer tmpFile.Close()
+
+  fileBytes, err := ioutil.ReadAll(file)
+  if err != nil {
+    fmt.Println("Error while reading user-uploaded file: ", err)
+    fmt.Println(err)
+  }
+
+  tmpFile.Write(fileBytes)
+  fmt.Println(w, "File uploaded.")
 }
 
 func setupRoutes() {
-  http.HandleFunc("/upload", uploadFile)
+  http.HandleFunc("/upload", uploadHandler)
+  http.HandleFunc("/download", downloadHandler)
   http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-  fmt.Println("Hello World")
+  setupRoutes()
 }
